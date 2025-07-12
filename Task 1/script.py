e26e667bd4c9f7b548f474cfe931d1ac1c93373a
Bhagyashri2005 <146646176+Bhagyashri2005@users.noreply.github.com>
2025-07-12 22:17:09 +0530

    script.py

--- Task 1/script.py
+++ Task 1/script.py
@@ -1,3 +1,10 @@
+'''Task 1 - Web Scraping
+● Use Python libraries like BeautifulSoup or Scrapy to extract data from websites.
+● Identify and collect relevant datasets from public web pages.
+● If you don’t code, use automated tools like Octoparse or ParseHub.
+● Learn to handle HTML structure and web navigation to gather accurate data.
+● Create custom datasets tailored to specific analysis needs.'''
+
 import requests
 from bs4 import BeautifulSoup
 import csv
